7fb422464cfe78b8dbcbc0eb797d2e7a42bb0028
Joaby Oliveira <joabyoliveira2004@gmail.com>
2022-11-12 08:19:54 -0300

    feat: create super class user

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
 <?php
+
+class Employee
+{
+  private $name;
+  private $enterprise;
+  private $position;
+  private $wage;
+
+
+  public function getName()
+  {
+    return $this->name;
+  }
+  public function getEnterprise()
+  {
+    return $this->enterprise;
+  }
+  public function getPosition()
+  {
+    return $this->position;
+  }
+  public function getWage()
+  {
+    return $this->wage;
+  }
+
+
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+  public function setEnterprise($enterprise)
+  {
+    $this->enterprise = $enterprise;
+  }
+  public function setPosition($position)
+  {
+    $this->position = $position;
+  }
+  public function setWage($wage)
+  {
+    $this->wage = $wage;
+  }
+}
